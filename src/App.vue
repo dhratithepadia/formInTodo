905df0f85153e8f53c2fec8fabf6bab2ebc4396b
DhratiThepadia <dhratithepedia@gmail.com>
2021-06-30 12:06:54 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+
+ <template>
+  <div>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-on:del-todo="deleteTodo" v-bind:todos="todos"/>
+  </div>
+</template>
+
+<script>
+import AddTodo from './components/AddTodo.vue'
+import Todos from './components/Todos.vue'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      todos: [
+        {
+          id: '1',
+          title: 'todo 1',
+          completed: false
+        },
+        {
+          id: '2',
+          title: 'todo 2',
+          completed: false
+        },
+        {
+          id: '3',
+          title: 'todo 3',
+          completed: false
+        },
+        {
+          id: '4',
+          title: 'todo 4',
+          completed: false
+        }
+      ]
+    }
+  },
+  components: {
+    AddTodo,
+    Todos
+  },
+  methods: {
+
+    addTodo (newTodo) {
+      this.todos = [...this.todos, newTodo]
+    },
+    deleteTodo (id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id)
+    }
+  }
+}
+</script>
